4dd0b223b1ec22e22b4fbdbb4ab6ec6e1409a113
shreyasvalmiki <shreyasvlamiki@gmail.com>
2013-12-16 00:54:49 -0500

    Merged Manas' changes

--- src/main/java/com/sifter/email/lib/StanfordResources.java
+++ src/main/java/com/sifter/email/lib/StanfordResources.java
@@ -23,100 +23,126 @@ import gate.util.Out;
  * @author : Kiran K.
  */
 public class StanfordResources {
-	
+
 	private LexicalizedParser lp = null;
 	private String tp;
-	
+
 	// This option shows loading and sentence-segmenting and tokenizing
-    // a file using DocumentPreprocessor.
-    private TreebankLanguagePack tlp;
-    private GrammaticalStructureFactory gsf;
-	
+	// a file using DocumentPreprocessor.
+	private TreebankLanguagePack tlp;
+	private GrammaticalStructureFactory gsf;
+
 	public StanfordResources()
 	{
 		lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
 		tlp = new PennTreebankLanguagePack();
 		gsf  = tlp.grammaticalStructureFactory();
 	}
-	
+
 	public void setThreadPart(String threadPart)
 	{
 		tp = threadPart;
 		Out.prln(threadPart);
 	}
-	
+
 	public String getThreadPart()
 	{
 		return tp;
 	}
-	
-	
+
+
 	public ArrayList<String> getPhrases(String text, String pos){
 		ArrayList<String> phrases = new ArrayList<String>();
 		TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
-	    List<CoreLabel> rawWords = tokenizerFactory.getTokenizer(new StringReader(text)).tokenize();
-	    Tree currTree = lp.apply(rawWords);
+		List<CoreLabel> rawWords = tokenizerFactory.getTokenizer(new StringReader(text)).tokenize();
+		Tree currTree = lp.apply(rawWords);
 		ArrayList<Tree> treeList = new ArrayList<Tree>();
-		getPhrases(treeList, currTree,pos);
-		
+		getPhrases(treeList, currTree);
+
 		for(Tree t : treeList){
 			Out.prln(t.toString());
 			Out.prln();
 		}
-		
-		
+
+
 		return phrases;
 	}
-	
 
-	
-	
-	private void getPhrases(ArrayList<Tree> treeList, Tree currTree, String pos){
+
+
+
+	//	private void getPhrases(ArrayList<Tree> treeList, Tree currTree, String pos){
+	//		if(currTree == null){
+	//			return;
+	//		}
+	//		else if(currTree.label().toString().equals(pos)){
+	//			treeList.add(currTree);
+	//		}
+	//		
+	//		else{
+	//			for(Tree t:currTree.getChildrenAsList()){
+	//				getPhrases(treeList,t,pos);
+	//			}
+	//		}
+	//	}
+
+
+	private void getPhrases(ArrayList<Tree> treeList, Tree currTree){
 		if(currTree == null){
+			//Out.prln(currTree.size());
 			return;
 		}
-		else if(currTree.label().toString().equals(pos)){
-			treeList.add(currTree);
-		}
-		
-		else{
-			for(Tree t:currTree.getChildrenAsList()){
-				getPhrases(treeList,t,pos);
+		for(Tree t:currTree.getChildrenAsList()){
+
+			if(t.label().toString().equals("VB") || t.label().toString().equals("PRP")) {
+				treeList.add(t);
+			}
+			else if(t.label().toString().equals("NNP") && Character.isUpperCase(t.toString().split(" ")[1].charAt(0))) {
+				treeList.add(t);
+			}
+			else if(t.label().toString().equals("NP") &&  t.size()>8 && t.size()<32){
+				treeList.add(t);
+			}
+			else if(t.label().toString().equals("VP") &&  t.size()>8 && t.size()<32){
+				treeList.add(t);
 			}
 		}
+		for(Tree t:currTree.getChildrenAsList()){
+			getPhrases(treeList,t);
+		}
 	}
-	
-	
+
+
 	public void parseThreadPart()
 	{
 		try
 		{
 			String parseSentence = getThreadPart();
-			
+
 			// This option shows loading and using an explicit tokenizer
-		    //TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
-		    //List<CoreLabel> rawWords2 = tokenizerFactory.getTokenizer(new StringReader(parseSentence)).tokenize();
-		    //Tree parse = lp.apply(rawWords2);
-		    //parse.
-		    TreebankLanguagePack tlp = new PennTreebankLanguagePack();
-		    //GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
-		    //GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
-		    
-		    //List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
-		    System.out.println();
-		    //System.out.println(tdl);
-		    System.out.println();
-		    
-		    Out.prln("******************NP*****************************");
-		    getPhrases(parseSentence,"NP");
-		    Out.prln("******************/NP*****************************");
-		    Out.prln();
-		    
-		    Out.prln("******************VP*****************************");
-		    getPhrases(parseSentence,"VP");
-		    Out.prln("******************/VP*****************************");
-		    //TreePrint tp = new TreePrint("penn,typedDependenciesCollapsed");
-		    //tp.printTree(parse);
+			//TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
+			//List<CoreLabel> rawWords2 = tokenizerFactory.getTokenizer(new StringReader(parseSentence)).tokenize();
+			//Tree parse = lp.apply(rawWords2);
+			//parse.
+			TreebankLanguagePack tlp = new PennTreebankLanguagePack();
+			//GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+			//GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
+
+			//List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
+			System.out.println();
+			//System.out.println(tdl);
+			System.out.println();
+
+			Out.prln("******************NP*****************************");
+			getPhrases(parseSentence,"NP");
+			Out.prln("******************/NP*****************************");
+			Out.prln();
+
+			Out.prln("******************VP*****************************");
+			getPhrases(parseSentence,"VP");
+			Out.prln("******************/VP*****************************");
+			//TreePrint tp = new TreePrint("penn,typedDependenciesCollapsed");
+			//tp.printTree(parse);
 		}
 		catch(Exception e)
 		{
